5ac75b5e0f7225cb8dccab4a6a38e0f8d0e9be54
schiller <schiller@gaolong.local>
2015-07-30 16:47:40 +0800

    新增Masonry 布局第三方库

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -96,6 +96,12 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
 
+// Masonry
+#define COCOAPODS_POD_AVAILABLE_Masonry
+#define COCOAPODS_VERSION_MAJOR_Masonry 0
+#define COCOAPODS_VERSION_MINOR_Masonry 6
+#define COCOAPODS_VERSION_PATCH_Masonry 2
+
 // UIView+Positioning
 #define COCOAPODS_POD_AVAILABLE_UIView_Positioning
 #define COCOAPODS_VERSION_MAJOR_UIView_Positioning 1
