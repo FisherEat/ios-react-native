73247b5bf676aa6b1ee2ce753b70ff33beff2eb1
gaolong <gaolong@tuniu.com>
2016-04-22 10:11:23 +0800

    【高龙】修正npm启动脚本

--- start.sh
+++ start.sh
@@ -6,7 +6,22 @@
 #
 ################
 
-cd
-cd /ios/MyGLDemos
-cd node_modules/react-native
-npm run start
+set -e
+set -x
+
+CURRENT_WORK_DIR=`echo ${0%/*}`
+
+if [ -d ${CURRENT_WORK_DIR} ];then
+   cd ${CURRENT_WORK_DIR}
+fi
+
+SCRIPT_PATH=`pwd`
+cd "${SCRIPT_PATH}"
+
+npm_run()
+{
+  cd node_modules/react-native
+  npm run start
+}
+
+npm_run
