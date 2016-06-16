9d09a944192d53c2afdca27ba2d8c757ed28c990
gaolong <gaolong@tuniu.com>
2016-06-16 11:12:47 +0800

    【高龙】创建头部滚动栏

--- reactjs/channel/SectionBanner.js
+++ reactjs/channel/SectionBanner.js
@@ -3,9 +3,139 @@
 */
 
 'use strict';
-import React, { AppRegistry } from 'react-native';
-var domestic = require("./js/modules/domestic");
-var abroad = require("./js/modules/abroad");
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Alert,
+  Linking,
+  Dimensions,
+  ScrollView,
+  ListView,
+  TouchableHighlight
+} from 'react-native';
+var ScreenWidth = Dimensions.get("window").width;
 
-AppRegistry.registerComponent('domestic', ()=> domestic);
-AppRegistry.registerComponent('abroad', ()=> abroad);
+class SectionBanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dotIndex: 0
+    }
+  }
+
+  render() {
+    var self = this;
+    var items = this.props.mItems;//从外部传来的参数
+    var scrollTimer = null;
+    var _ = this;
+    var scrollEnd = function (x) {
+      var index = x / ScreenWidth;
+      if (index - Math.floor(x) > 0.5) {
+        index = Math.ceil(index);
+      }else {
+        index = Math.floor(index);
+      }
+      _.setState({
+        dotIndex: index
+      });
+    }
+    var scroll = function (event) {
+      clearTimeout(scrollTimer);
+      var x = event.nativeEvent.contentOffset.x;
+      var index = x / ScreenWidth;
+      if (index - Math.floor(index) > 0.5) {
+        index = Math.ceil(index);
+      }else {
+        index = Math.floor(index);
+      }
+      _.setState({
+        dotIndex: index
+      });
+    }
+
+    return (
+      <View>
+        <ScrollView
+           onScroll={scroll}
+           scrollEventThrottle={16}
+           contentOffset={{x: 0, y: 0}}
+           pagingEnable={true}
+           horizontal={true}
+           contentContainerStyle={styles.slide}
+           showsHorizontalScrollIndicator={false}
+           directionalLockEnabled={false}
+           key="slideBox">
+           {
+             items.map((item, index) => {
+               return (
+                 <TouchableHighlight
+                   onPress={()=>{}}
+                   key={"slide_pic" + index}>
+                   <Image source={{uri: item.adImgUrl}}
+                          style={styles.slideImage}
+                          resizeMode="cover"/>
+                 </TouchableHighlight>
+               )
+             })
+           }
+        </ScrollView>
+        <View style={styles.dotContainer}
+              key="slideDotBox">
+              {
+                items.map((item, index) => {
+                  return (
+                    <View style={index==_.state.dotIndex ? sytles.slideDotActive : styles.slideDot}
+                          key={"slideDot"+index}>
+                    </View>
+                  )
+                })
+              }
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  slide: {
+    backgroundColor: '#ccc',
+    alignItems:"flex-start",
+    alignSelf: 'flex-start',
+    flexDirection: 'row'
+  },
+  slideDot: {
+    width: 4,
+    height: 4,
+    borderRadius: 4,
+    backgroundColor: '#fff',
+    margin: 4,
+  },
+  slideDotActive: {
+    width: 4,
+    height: 4,
+    borderRadius: 4,
+    backgroundColor: "#ff8a00",
+    margin: 4
+  },
+  dotContainer: {
+       height: 4,
+       width: ScreenWidth,
+       top: -10,
+       left: 0,
+       flex: 1,
+       alignItems: "center",
+       justifyContent: "center",
+       flexDirection: "row"
+   },
+   slideImage: {
+       width: ScreenWidth,
+       height: ScreenWidth / 750 * 230,
+   }
+});
+
+export default SectionBanner;
--- reactjs/modules/domestic.ios.js
+++ reactjs/modules/domestic.ios.js
@@ -15,6 +15,7 @@ import React, {
   ListView,
   NativeModules
 } from 'react-native';
+import SectionBanner from '../channel/SectionBanner'
 
 let ScreenWidth = Dimensions.get("window").width;
 let ScreenHeight = Dimensions.get("window").height;
@@ -32,18 +33,28 @@ class Domestic extends Component {
     var _ = this;
     let url = "http://m.tuniu.com/event/admin/getCmsChannelAjax?pageId=1209&cityCode=";
 
-    fetch(url+cityInfo.code).then((response) => {
-        return response.json();
-     }).then((json) => {
+    fetch(url+cityInfo.code).then((response) => response.json()).then((json) => {
       _.setState({
         pageStatus: json.data
       });
     })
   }
 
+  onScroll(e) {
+         var _ = this;
+         this.scrollListen.forEach((fn)=> {
+             var offsetY = e.nativeEvent.contentOffset.y;
+             _.refs.rootView.refs.InnerScrollView.measure((ox, oy, w, h) => {
+                 fn && fn(h - screenHeight - offsetY);
+             })
+
+         })
+  }
+
   componentDidMount() {
       let cityInfo = {code: 2500, name: '上海'};
       this.getData(cityInfo);
+      console.log('fuck'+this.state.pageStatus);
   }
 
   renderPage() {
@@ -52,7 +63,7 @@ class Domestic extends Component {
     var tabs = [];
     var sectionCount = 0;
 //data数据结构
-    var sections = data.map((d, index) => {
+    var sections = data.map(function (d, index) {
         sectionCount ++;
         if (d.gGroupId != 0) {
             tabs.push(d);
@@ -60,34 +71,47 @@ class Domestic extends Component {
         return (
             <View key={"react_group" + index}>
               {
-                  d.gItems.map((dd, index) => {
-                      if (dd.mTplId == 1537) {//展示标题栏
-                          return <View></View>
-                      }
+                  d.gItems.map(function (dd, index) {
+                      // if (dd.mTplId == 1537) {//展示标题栏
+                      //     return <View key={"Title"+index}></View>
+                      // }
+                      console.log(dd.mItems);
                       if (dd.mTplId == 1152) {//展示分页滚动条
-                          return <View></View>
+                          return <SectionBanner key={"SectionBanner"+index}
+                          mItems={dd.mItems}></SectionBanner>
                       }
-                      if (dd.mTplId == 1539) {//展示特卖产品
-                          return <View></View>
-                      }
-                      if (dd.mTplId == 1531) {//展示热门目的地
-                          return <View></View>
-                      }
-                      if (dd.mTplId == 1532) {//展示目的地
-                          return <View></View>
-                      }
+                      // if (dd.mTplId == 1539) {//展示特卖产品
+                      //     return <View key={"SpecialProduct"+index}>
+                      //              <Text>展示特卖产品</Text>
+                      //            </View>
+                      // }
+                      // if (dd.mTplId == 1531) {//展示热门目的地
+                      //     return <View key={"HotDest" + index}>
+                      //              <Text>展示热门目的地</Text>
+                      //            </View>
+                      // }
+                      // if (dd.mTplId == 1532) {//展示目的地
+                      //     return <View key={"SectionServer"+index}>
+                      //               <Text>展示目的地</Text>
+                      //            </View>
+                      // }
                   })
               }
             </View>
         )
     });
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeTip}>拿到线上数据了...</Text>
+  return (<View style={styles.container} key="react_loaded">
+        <ScrollView
+         ref="rootView"
+         scrollEventThrottle={100}
+         onScroll={_.onScroll.bind(_)}
+         style={styles.container}
+         key="react_abroad">
+            <Text>mabi</Text>
+        </ScrollView>
     </View>
     )
   }
-
   render() {
     if (!this.state.pageStatus) {
       return (
